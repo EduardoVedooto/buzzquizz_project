4c9ec89104abe89d9cb281ec72384e0e97d9f001
Yann Melo <yann.melo@gmail.com>
2021-04-19 22:29:19 -0300

    Js coment

--- Javascript/js.js
+++ Javascript/js.js
@@ -1,34 +1,34 @@
 
 
-// getQuizzes();
+getQuizzes();
 
-// function getQuizzes(){
-//     const promess = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/buzzquizz/quizzes");
-//     promess.then(showQuizzes);
-// }
+function getQuizzes(){
+    const promess = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/buzzquizz/quizzes");
+    promess.then(showQuizzes);
+}
 
-// function showQuizzes(response){
-//     console.log(response.data)
-//     const eachQuizz = document.querySelector(".quizzes");
+function showQuizzes(response){
+    console.log(response.data)
+    const eachQuizz = document.querySelector(".quizzes");
 
-//     eachQuizz.innerHTML = "";
-//     for(let i = 0; i < response.data.length; i++){
-//         eachQuizz.innerHTML +=`
-//         <div class="e-quizzes" onclick="acessQuizz(this)">
-//             <img src="${response.data[i].image}">
-//             <p>${response.data[i].title}</p>
-//         </div>  
-//         `
-//     }
-// }
+    eachQuizz.innerHTML = "";
+    for(let i = 0; i < response.data.length; i++){
+        eachQuizz.innerHTML +=`
+        <div class="e-quizzes" onclick="acessQuizz(this)">
+            <img src="${response.data[i].image}">
+            <p>${response.data[i].title}</p>
+        </div>  
+        `
+    }
+}
  
-// function createQuizz(clicked){
-//     const containerQuizz = document.querySelector(".container");
-//     // const createFeature = document.querySelector(".") missing feature;
-//     containerQuizz.classList.add("hidden");
+function createQuizz(clicked){
+    const containerQuizz = document.querySelector(".container");
+    // const createFeature = document.querySelector(".") missing feature;
+    containerQuizz.classList.add("hidden");
     
-// }
+}
 
-// function acessQuizz(clicked){
-//     alert("Feature não implementada")
-// }
+function acessQuizz(clicked){
+    alert("Feature não implementada")
+}
